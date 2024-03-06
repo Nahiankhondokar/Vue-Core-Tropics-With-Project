5f8bfb310e13854d67c17e5e5464b3014dc07fe6
Abdullah Al Nahian <info.nahian13@gmail.com>
2024-03-06 19:18:33 +0600

    emit process in compositional API

---
+++ src/components/Textarea.vue
@@ -0,0 +1,17 @@
+<script setup>
+import { defineProps, defineEmits } from 'vue'
+
+defineProps({
+  modelValue: String
+})
+
+let emit = defineEmits(['update:modelValue'])
+
+function update(e) {
+  emit('update:modelValue', e.target.value)
+}
+</script>
+
+<template>
+  <textarea v-text="modelValue" @keyup="update"></textarea>
+</template>
